d0e6e43b3be6b20d9d8200c6b6a95030a62afcfb
Helios <adealumnonegligas@gmail.com>
2023-02-13 21:55:16 -0600

    todo centrado y flex, flex wrap para los componentes dentro del componente principal App

--- src/App.js
+++ src/App.js
@@ -4,7 +4,11 @@ import Testimonio from "./componentes/Testimonio.jsx";
 function App() {
   return (
     <div className="App">
-      <Testimonio/>
+      <div className="contenedor-principal">
+        <h1>Esto es lo que dicen nuestros alumnos sobre freeCodeCamp:</h1>
+        <Testimonio/>
+      </div>
+
     </div>
   );
 }
