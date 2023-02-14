e45d42868a19b9c73ac5981234fc07ef3427f02e
Helios <adealumnonegligas@gmail.com>
2023-02-13 22:20:51 -0600

    dinamizando la imagen para el Testimonio

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
         <Testimonio
         nombre="Emma Bostian"
         pais="Suecia"
-        imagen=""
+        imagen="Emma"
         cargo="Ingeniera de Software"
         empresa="Spotify"
         testimonio='Siempre he tenido problemas para aprender JavaScript. He tomado muchos cursos, pero el curso de freeCodeCamp fue el que se quedó. Estudiar JavaScript, así como estructuras de datos y algoritmos en freeCodeCamp me dio las habilidades y la confianza que necesitaba para conseguir el trabajo de mis sueños como ingeniero de software en Spotify.'
