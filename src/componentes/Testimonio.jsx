6cf5d845356786dc948c15d8cd2c61286c46cb07
Helios <adealumnonegligas@gmail.com>
2023-02-13 22:12:35 -0600

    adding props al componente Testimonio para dinamizar el contenido de los parrafos

--- src/componentes/Testimonio.jsx
+++ src/componentes/Testimonio.jsx
@@ -1,16 +1,16 @@
 import React from "react";
 import "../hojas-de-estilo/Testimonio.css";
 
-function Testimonio() {
+function Testimonio(props) {
   return (
     <div className="contenedor-testimonio">
       <img className="imagen-testimonio"
         src={require("../imagenes/testimonio-Emma.png")}
         alt="Foto de Emma"/>
       <div className="contenedor-texto-testimonio">
-        <p className="nombre-testimonio">Emma Bostian en Suecia</p>
-        <p className="cargo-testimonio">Ingeniera de Software en Spotify</p>
-        <p className="texto-testimonio">"Siempre he tenido problemas para aprender JavaScript. He tomado muchos cursos, pero el curso de freeCodeCamp fue el que se quedó. Estudiar JavaScript, así como estructuras de datos y algoritmos en freeCodeCamp me dio las habilidades y la confianza que necesitaba para conseguir el trabajo de mis sueños como ingeniero de software en Spotify."</p>
+        <p className="nombre-testimonio">{props.nombre} en {props.pais}</p>
+        <p className="cargo-testimonio">{props.cargo} en {props.empresa}</p>
+        <p className="texto-testimonio">{props.testimonio}</p>
       </div>
     </div>
   );
